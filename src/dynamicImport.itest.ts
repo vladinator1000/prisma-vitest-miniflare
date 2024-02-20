71a272da95569765d5b5f22193810ba416392b8d
Vlady Veselinov <vladkodmc@gmail.com>
2024-02-20 13:23:00 +0000

    remove noise

--- src/dynamicImport.itest.ts
+++
@@ -1,9 +0,0 @@
-describe('dynamic import', () => { 
-  test('can get Prisma dynamically ', async () => { 
-    const {PrismaClient} = await import('@prisma/client')
-    const prisma = new PrismaClient()
-    expect(prisma).toBeTruthy()
-
-    await prisma.$disconnect()
-  })
-})
