3a2761125092a0af911d44762c5ae3179416bd8e
Vlady Veselinov <vladkodmc@gmail.com>
2024-02-20 13:21:23 +0000

    add a realistic integration test

--- src/user.itest.ts
+++ src/user.itest.ts
@@ -3,12 +3,17 @@ import { PrismaClient } from "@prisma/client"
 describe('user model', () => { 
   const prisma = new PrismaClient()
 
+  beforeEach(async () => {
+    await prisma.$queryRawUnsafe(`TRUNCATE "User";`)
+  })
+  
+  afterAll(async () => {
+    await prisma.$disconnect()
+  })
+
   test('create user', async () => { 
     const user = await prisma.user.create({data: {name: 'Paul Atreides'}})
 
-    expect(user).toEqual({
-      id: 1,
-      name: 'Paul Atreides'
-    })
+    expect(user.name).toEqual('Paul Atreides')
   })
 })
