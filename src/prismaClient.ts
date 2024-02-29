80562e86a7440658d2ac1a3d9dc325cfbd4a0218
Vlady Veselinov <vladkodmc@gmail.com>
2024-02-29 19:14:01 +0000

    add new line

--- src/prismaClient.ts
+++ src/prismaClient.ts
@@ -6,7 +6,7 @@ export function getPrisma(databaseUrl?: string) {
   if (!databaseUrl) {
     throw new Error('Missing databaseUrl')
   }
-  
+
   const pool = new pg.Pool({ connectionString: databaseUrl })
   const adapter = new PrismaPg(pool)
   const prisma = new PrismaClient({ adapter })
